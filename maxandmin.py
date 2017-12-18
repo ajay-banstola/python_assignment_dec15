e1de35e92d36e7c13c05f1c31a9689c48980c4bd
ajay-banstola <ajay.banstola@gmail.com>
2017-12-18 05:57:04 +0545

    made program to find max and min

--- maxandmin.py
+++ maxandmin.py
@@ -1,4 +1,4 @@
 my_list = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45]
 
 my_list.sort()
-print("The Maximum is {} and minimum is {}".format(my_list[-1],my_list[0]))
+print("The Maximum is {} and the minimum is {}".format(my_list[-1],my_list[0]))
